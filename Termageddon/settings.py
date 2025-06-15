1650442ddabc124d65d0d6440eb384c0227f94c9
David Wolfe <davidgameswolfe@gmail.com>
2025-06-15 17:14:48 -0300

    Repair filtering
    Good nice progress.  I note that the filter box "Search terms..." no longer filters.  In particular, I can see get requests with the get parameter search, but I don't think the backend is transmitting the filter directive to the database query.

--- Termageddon/settings.py
+++ Termageddon/settings.py
@@ -136,5 +136,8 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ],
 }
