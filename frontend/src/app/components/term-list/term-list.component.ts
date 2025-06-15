9794c2a81fef2234a95d51e9691174205973a83f
David Wolfe <davidgameswolfe@gmail.com>
2025-06-15 17:30:24 -0300

    fixed header and searchbar
    Nice.  Let's fix the header with the title and logout as well as the search terms input box.  There should be a single scrollbar which affects the rest of the page below the search box.

--- frontend/src/app/components/term-list/term-list.component.ts
+++ frontend/src/app/components/term-list/term-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { GlossaryService, Term } from '../../services/glossary.service';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-term-list',
@@ -12,33 +14,58 @@ import { RouterModule } from '@angular/router';
   templateUrl: './term-list.component.html',
   styleUrls: ['./term-list.component.scss']
 })
-export class TermListComponent implements OnInit {
+export class TermListComponent implements OnInit, OnDestroy {
   terms: Term[] = [];
   page = 1;
   hasMore = true;
   loading = false;
   searchQuery = '';
 
+  private searchSubject = new Subject<string>();
+  private searchSubscription!: Subscription;
+
   constructor(private glossaryService: GlossaryService) { }
 
   ngOnInit(): void {
     this.loadTerms();
+
+    this.searchSubscription = this.searchSubject
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(() => {
+        this.loadTerms(true);
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.searchSubscription) {
+      this.searchSubscription.unsubscribe();
+    }
   }
 
   loadTerms(fromSearch: boolean = false): void {
-    if (this.loading || !this.hasMore) {
+    if (this.loading && !fromSearch) {
+      return;
+    }
+    if (!this.hasMore && !fromSearch) {
       return;
     }
 
     if (fromSearch) {
       this.page = 1;
-      this.terms = [];
       this.hasMore = true;
     }
 
     this.loading = true;
     this.glossaryService.getTerms(this.page, this.searchQuery).subscribe(response => {
-      this.terms.push(...response.results);
+      if (fromSearch) {
+        this.terms = response.results;
+      } else {
+        this.terms.push(...response.results);
+      }
+      
       this.page++;
       this.hasMore = response.next !== null;
       this.loading = false;
@@ -52,6 +79,6 @@ export class TermListComponent implements OnInit {
   }
   
   onSearch(): void {
-    this.loadTerms(true);
+    this.searchSubject.next(this.searchQuery);
   }
 } 
