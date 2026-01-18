95bd21459f883aa7f07496a89595142d5060edc8
David Wolfe <davidgameswolfe@gmail.com>
2026-01-18 09:09:44 -0400

    Nix empty tf file

--- terraform/aws/modules/9_cloudwatch.tf
+++
@@ -1,5 +0,0 @@
-# CloudWatch log group is already defined in 4_ecs.tf
-# Removing duplicate to avoid conflict
-
-# If you have other CloudWatch resources, keep them here
-# Otherwise, you can delete this file
