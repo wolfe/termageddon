fa4afc15afdccf639cd55dd72d30fe0fff24d74d
David Wolfe <davidgameswolfe@gmail.com>
2026-01-28 15:01:18 -0400

    Simplify the health check done by the ECS deploy

--- terraform/aws/modules/4_ecs.tf
+++ terraform/aws/modules/4_ecs.tf
@@ -182,14 +182,6 @@ resource "aws_ecs_task_definition" "backend" {
         }
       }
 
-      healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:8000/api/health/ || exit 1"]
-        interval    = 30
-        timeout     = 5
-        retries     = 3
-        startPeriod = 60
-      }
-
       essential = true
     }
   ])
